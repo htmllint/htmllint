d96cdb4d0368eed0a81d54f5d8f2b8939c637b3d
Coalman <tehcoalman@gmail.com>
2014-08-29 14:07:13 -0400

    added basic rule forbidding inline styles

---
+++ test/rules.inline_style.js
@@ -0,0 +1,39 @@
+var expect = require('chai').expect;
+
+describe('rules.inline_style', function () {
+    var inlineStyle = require('../lib/rules/inline_style');
+
+    it('should be an object', function () {
+        expect(inlineStyle).to.be.an.instanceOf(Object);
+    });
+
+    it('should have a name', function () {
+        expect(inlineStyle).to.have.property('name');
+    });
+
+    it('should have a description', function () {
+        expect(inlineStyle).to.have.property('description');
+    });
+
+    describe('process', function () {
+        it('should return an array', function () {
+            var output = inlineStyle.process('');
+
+            expect(output).to.be.an.instanceOf(Array);
+        });
+
+        it('should match style elements', function () {
+            var html = '<style></style>',
+                output = inlineStyle.process(html);
+
+            expect(output).to.have.length(1);
+        });
+
+        it('should match style attributes', function () {
+            var html = '<button style=""></button>',
+                output = inlineStyle.process(html);
+
+            expect(output).to.have.length(1);
+        });
+    });
+});
