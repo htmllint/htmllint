ee2be73abf68c88edc78b6ae63514a81b2398d96
Coalman <tehcoalman@gmail.com>
2014-09-08 01:26:34 -0400

    added tests to get us back to 100% test coverage

---
+++ test/parser.dom_builder.js
@@ -0,0 +1,20 @@
+var expect = require('chai').expect;
+
+describe('parser.dom_builder', function () {
+    var DomBuilder = require('../lib/parser/dom_builder'),
+        DomHandler = require('htmlparser2').DomHandler;
+
+    it('should be a constructor', function () {
+        expect(DomBuilder).to.be.an.instanceOf(Function);
+    });
+
+    it('should inherit from DomHandler', function () {
+        expect(DomBuilder.prototype).to.be.an.instanceOf(DomHandler);
+    });
+
+    it('should throw on addDomElement if not initialized with a parser', function () {
+        var builder = new DomBuilder();
+
+        expect(builder._addDomElement.bind(builder)).to.throw(Error);
+    });
+});
