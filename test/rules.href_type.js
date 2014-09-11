c2bbd4b32ad9d41424650e85147650334208f831
Roman <romnempire@gmail.com>
2014-09-11 11:18:58 -0400

    Adds rule href-type.  the tests are incomplete, the code needs linting. willfix, just wanted to push something.
    Signed-off-by: Roman <romnempire@gmail.com>

---
+++ test/rules.href_type.js
@@ -0,0 +1,37 @@
+describe('rules.href_type', function() {
+
+	var rule = require('../lib/rules/href_type'),
+		htmllint = require('../');
+
+	var Parser = require('../lib/parser'),
+		parser = null;
+
+    describe('process', function () {
+        var Parser = require('../lib/parser'),
+            parser = null;
+
+        it('should return an array', function () {
+            var output = rule.process([]);
+
+            expect(output).to.be.an.instanceOf(Array);
+        });
+        //this code is for if the flag is set to absolute
+        it('should not match absolute links', function () {
+
+            var parser = new Parser(),
+            	dom = parser.parse('<a href="http://www.google.com"></a>'),
+                output = rule.process(dom, {"href-type":"absolute"});
+
+            expect(output).to.have.length(0);
+        });
+
+        it('should match relative links', function () {
+            var parser = new Parser(),
+            	dom = parser.parse('<a href="/dog/cat"></a>'),
+                output = rule.process(dom, {"href-type":"absolute"});
+
+            expect(output).to.have.length(1);
+        });
+    });
+
+});
