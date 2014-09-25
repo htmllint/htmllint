9299b91cdc4767a3a2a3a52defd599ddbb978d50
Coalman <tehcoalman@gmail.com>
2014-09-25 18:57:27 -0400

    removing old test

--- test/functional/disallowed-tags.js
+++ test/functional/disallowed-tags.js
@@ -4,10 +4,6 @@ module.exports = [
         input: '<body><div><p>This is text yo</p></div></body>',
         output: 0
     }, {
-        desc: 'should match style by default',
-        input: '<body><div><style>p {color: red;}</style></div></body>',
-        output: 1
-    }, {
         desc: 'should match options if user sets them',
         input: '<body><button style=""></button><main></main></body>',
         opts: { 'disallowed-tags': ['button', 'main'] },
