dd376154d032642a42742d3b94c791ca81aea98c
Marshall Lochbaum <mwlochbaum@gmail.com>
2014-09-29 00:20:41 -0400

    Add testing for id-unique on non-tags

--- test/functional/id-unique.js
+++ test/functional/id-unique.js
@@ -1,17 +1,17 @@
 module.exports = [
     {
         desc: 'should pass html with no duplicate ids',
-        input: '<div id="div1"></div><div id="div2"></div>',
+        input: '<div id="div1"></div>text<div id="div2"></div>',
         opts: { 'id-unique': true },
         output: 0
     }, {
         desc: 'should fail html with a duplicate id',
-        input: '<div id="div1"></div><div id="div1"></div>',
+        input: '<div id="div1"></div>text<div id="div1"></div>',
         opts: { 'id-unique': true },
         output: 1
     }, {
         desc: 'should pass any html when disabled',
-        input: '<div id="div1"></div><div id="div1"></div>',
+        input: '<div id="div1"></div>text<div id="div1"></div>',
         opts: { 'id-unique': false },
         output: 0
     }
