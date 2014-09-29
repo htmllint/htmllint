005043793b3e33e2963d7459b5bb22763d0cf008
messman <andrewmess93@gmail.com>
2014-09-29 11:00:38 -0400

    Fixed rules_indent
    Closed #1. Also fixed some linting issues and added rows to the line
    objects

--- test/functional/indent-style.js
+++ test/functional/indent-style.js
@@ -18,16 +18,69 @@ module.exports = [
         opts: { 'indent-style': 'spaces' },
         output: 1
     }, {
-        desc: 'should match mixed indents',
+        desc: 'should match tabs when configured for spaces',
+        input: [
+            '<body>',
+            '\t<p> ello</p>',
+            '\t<p> ello</p>',
+            '\t<p> ello</p>',
+            '</body>'
+        ].join('\n'),
+        opts: { 'indent-style': 'spaces' },
+        output: 3
+    },
+    {
+        desc: 'should match inmixed indents',
         input: [
             '<body>',
             '\t <p>this not okay</p>',
             '</body>'
         ].join('\n'),
+        opts: { 'indent-style': 'spaces' },
+        output: 1
+    }, {
+        desc: 'should match mixed indents starting with spaces',
+        input: [
+            '<body>',
+            '    <p>this not okay</p>',
+            '    <p>this not okay</p>',
+            '\t\t\t\t<p>this not okay</p>',
+            '\t\t\t\t<p>this not okay</p>',
+            '</body>'
+        ].join('\n'),
         opts: { 'indent-style': 'nonmixed' },
+        output: 2
+    }, {
+        desc: 'should match mixed indents starting with tabs',
+        input: [
+            '<body>',
+            '\t\t<p>this not okay</p>',
+            '\t\t<p>this not okay</p>',
+            '      <div>Hey</div>',
+            '</body>'
+        ].join('\n'),
+        opts: { 'indent-style': 'nonmixed' },
+        output: 1
+    }, {
+        desc: 'should not match on false',
+        input: [
+            '<body>',
+            '\t\t<p>this not okay</p>',
+            '\t\t<p>this not okay</p>',
+            '      <div>Hey</div>',
+            '</body>'
+        ].join('\n'),
+        opts: { 'indent-style': 'false' },
+        output: 0
+    }, {
+        desc: 'should match mixed indents by default',
+        input: [
+            '<body>',
+            '\t\t<p>this not okay</p>',
+            '\t\t<p>this not okay</p>',
+            '      <div>Hey</div>',
+            '</body>'
+        ].join('\n'),
         output: 1
     }
 ];
-
-// TODO: remove pending tests
-module.exports = [];
--- test/functional/tag-name-lowercase.js
+++ test/functional/tag-name-lowercase.js
@@ -3,5 +3,15 @@ module.exports = [
         desc: 'should match attributes with mixed case',
         input: '<boDY>',
         output: 1
+    },
+    {
+        desc: 'should not match attributes with lowered case',
+        input: '<body>',
+        output: 0
+    },
+    {
+        desc: 'should multiple mixed-case elements',
+        input: '<HTML><seCtion></section></HTML>',
+        output: 2
     }
 ];
