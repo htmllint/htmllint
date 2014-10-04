cff3493bde5e9399e0e9bf58c7593763585d9b37
Coalman <tehcoalman@gmail.com>
2014-10-04 17:27:19 -0400

    added label_for impl, closes #32

---
+++ test/functional/label-for.js
@@ -0,0 +1,64 @@
+module.exports = [
+    {
+        desc: 'should pass valid for attr in strict mode',
+        input: [
+            '<label for="thing">The Thing</label>',
+            '<input type="text" id="thing"/>'
+        ].join(''),
+        opts: { 'label-for': 'strict' },
+        output: 0
+    }, {
+        desc: 'should fail label with only child in strict mode',
+        input: [
+            '<label>',
+            '<input type="text" id="thing"/>',
+            '</label>'
+        ].join(''),
+        opts: { 'label-for': 'strict' },
+        output: 1
+    }, {
+        desc: 'should pass label with only child in nonstrict mode',
+        input: [
+            '<label>',
+            '<input type="text" id="thing"/>',
+            '</label>'
+        ].join(''),
+        opts: { 'label-for': true },
+        output: 0
+    }, {
+        desc: 'should fail label with nonexistant for id',
+        input: [
+            '<label for="noexist">',
+            '<input type="text" id="thing"/>',
+            '</label>'
+        ].join(''),
+        opts: { 'label-for': true },
+        output: 1
+    }, {
+        desc: 'should pass label for with child id',
+        input: [
+            '<label for="thing">',
+            '<input type="text" id="thing"/>',
+            '</label>'
+        ],
+        opts: { 'label-for': true },
+        output: 0
+    }, {
+        desc: 'should fail an unlabeable child in nonstrict',
+        input: [
+            '<label>',
+            '<p>not labeable</p>',
+            '</label>'
+        ],
+        opts: { 'label-for': true },
+        output: 1
+    }, {
+        desc: 'should fail an id that points to an unlabeable element',
+        input: [
+            '<label for="para"></label>',
+            '<div id="para"></div>'
+        ],
+        opts: { 'label-for': true },
+        output: 1
+    }
+];
