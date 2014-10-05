db0d683ab1f7d9783961ee152694a8b329d40c75
Coalman <tehcoalman@gmail.com>
2014-10-05 19:35:35 -0400

    added parse method to repl

--- repl.js
+++ repl.js
@@ -5,3 +5,5 @@ var ctx = repl.start('>> ').context;
 
 // export stuff to use in the repl
 ctx.lint = require('./');
+var parser = ctx.lint.defaultLinter.parser;
+ctx.parse = parser.parse.bind(parser);
