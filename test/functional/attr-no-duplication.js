594aaddd0aa6b162900d0093395c58e7ba07af1f
messman <andrewmess93@gmail.com>
2014-10-08 09:22:15 -0400

    Added files for attr-no-duplication
    DOES NOT WORK. Committing so I can easily see the diff later.

---
+++ test/functional/attr-no-duplication.js
@@ -0,0 +1,28 @@
+module.exports = [
+    {
+        desc: 'should pass when set to false',
+        input: '<section><div><p>Hello</p><p>World</p></div></section>',
+        opts: { 'doctype-first': false },
+        output: 0
+    }, {
+        desc: 'should pass when !DOCTYPE is first',
+        input: '<!DOCTYPE><div><p>Hello</p><p>World</p></div>',
+        opts: { 'doctype-first': true },
+        output: 0
+    }, {
+        desc: 'should fail when !DOCTYPE is not present',
+        input: '<section><div><p>Hello</p><p>World</p></div></section>',
+        opts: { 'doctype-first': true },
+        output: 1
+    }, {
+        desc: 'should fail when !DOCTYPE is not first',
+        input: '<section><!DOCTYPE html><div><p>Hello</p><p>World</p></div></section>',
+        opts: { 'doctype-first': true },
+        output: 1
+    }, {
+        desc: 'should pass when multiple !DOCTYPEs are present, as long as a !DOCTYPE is first',
+        input: '<!DOCTYPE><section><!DOCTYPE><div><p>Hello</p><p>World</p></div></section>',
+        opts: { 'doctype-first': true },
+        output: 0
+    }
+];
