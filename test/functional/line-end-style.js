c840ce3edca29ac81da72b55513eb8d0d66fc6b3
Marshall Lochbaum <mwlochbaum@gmail.com>
2014-10-14 22:07:30 -0400

    Rename line-ending to line-end-style

--- test/functional/line-end-style.js
+++ test/functional/line-end-style.js
@@ -11,22 +11,22 @@ module.exports = [
     {
         desc: 'should match CR and CRLF when set to LF',
         input: input,
-        opts: { 'line-ending': 'lf' },
+        opts: { 'line-end-style': 'lf' },
         output: 3
     }, {
         desc: 'should match LF and CRLF when set to CR',
         input: input,
-        opts: { 'line-ending': 'cr' },
+        opts: { 'line-end-style': 'cr' },
         output: 4
     }, {
         desc: 'should match CR and LF when set to CRLF',
         input: input,
-        opts: { 'line-ending': 'crlf' },
+        opts: { 'line-end-style': 'crlf' },
         output: 5
     }, {
         desc: 'should not match anything when set to false',
         input: input,
-        opts: { 'line-ending': false },
+        opts: { 'line-end-style': false },
         output: 0
     }
 ];
