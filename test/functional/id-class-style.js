aa7b7ca5ac936b043f2244595ad2a9142b769eba
Marshall Lochbaum <mwlochbaum@gmail.com>
2014-10-24 12:42:40 -0400

    Rename id-class-value to id-class-style

--- test/functional/id-class-style.js
+++ test/functional/id-class-style.js
@@ -2,27 +2,27 @@ module.exports = [
     {
         desc: 'should pass when set to false',
         input: '<div id="2fOwj_0o-3" class="0r9jfFJ2"></div>',
-        opts: { 'id-class-value': false },
+        opts: { 'id-class-style': false },
         output: 0
     }, {
         desc: 'should pass correctly styled id and class',
         input: '<div id="abc" class="fowj0wo3"></div>',
-        opts: { 'id-class-value': 'lowercase' },
+        opts: { 'id-class-style': 'lowercase' },
         output: 0
     }, {
         desc: 'should fail incorrectly styled id names',
         input: '<div id="foWj0wo3"></div>',
-        opts: { 'id-class-value': 'lowercase' },
+        opts: { 'id-class-style': 'lowercase' },
         output: 1
     }, {
         desc: 'should fail incorrectly styled class names',
         input: '<div class="fojf*ovo"></div>',
-        opts: { 'id-class-value': 'lowercase' },
+        opts: { 'id-class-style': 'lowercase' },
         output: 1
     }, {
         desc: 'should accept a "dash" option',
         input: '<div id="abc" class="fowj-awo3-fqowj"></div>',
-        opts: { 'id-class-value': 'dash' },
+        opts: { 'id-class-style': 'dash' },
         output: 0
     }
 ];
