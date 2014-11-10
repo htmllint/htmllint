67a5b42a97f74ae1fd81850967798b3a25ee0946
Coalman <tehcoalman@gmail.com>
2014-11-09 22:38:27 -0500

    patched repl, fixes #66

--- repl.js
+++ repl.js
@@ -3,7 +3,21 @@
 var repl = require('repl');
 var ctx = repl.start('>> ').context;
 
+var htmllint = require('./');
+
 // export stuff to use in the repl
-ctx.lint = require('./');
-var parser = ctx.lint.defaultLinter.parser;
+ctx.htmllint = htmllint;
+
+ctx.lint = function () {
+    var promise = ctx.htmllint.apply(ctx.htmllint, arguments);
+
+    promise.then(function (issues) {
+        ctx['_'] = issues;
+
+        console.log(issues);
+    });
+};
+
+
+var parser = ctx.htmllint.defaultLinter.parser;
 ctx.parse = parser.parse.bind(parser);
