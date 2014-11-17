d7d7a168a5b24b3ea37316f622c49af21bf54dc3
Marshall Lochbaum <mwlochbaum@gmail.com>
2014-11-16 23:14:50 -0500

    Add tag-name-match rule

---
+++ test/functional/tag-name-match.js
@@ -0,0 +1,23 @@
+module.exports = [
+    {
+        desc: 'should pass when set to false',
+        input: '<HTML><seCtion></section></HtML>',
+        opts: { 'tag-name-match': false },
+        output: 0
+    }, {
+        desc: 'should pass for matching tags',
+        input: '<boDY></boDY>',
+        opts: { 'tag-name-match': true },
+        output: 0
+    }, {
+        desc: 'should fail for non-matching tags',
+        input: '<boDY></bODy>',
+        opts: { 'tag-name-match': true },
+        output: 1
+    }, {
+        desc: 'should fail for each non-matching set',
+        input: '<HTML><section></seCtion></html>',
+        opts: { 'tag-name-match': true },
+        output: 2
+    }
+];
