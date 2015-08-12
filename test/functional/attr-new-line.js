ddabcd05690cd4ad8e6aed4e0dc1b511c78c2cb5
Oleksandr Knyga <oleksandrknyga@gmail.com>
2015-08-12 15:28:50 +0200

    add +0 mode

--- test/functional/attr-new-line.js
+++ test/functional/attr-new-line.js
@@ -18,21 +18,33 @@ module.exports = [
         output: 0
     },
     {
+        desc: 'should not match tags with attributes splitted on multiple lines if first row has attributes less or equal then provided in rule',
+        input: '<div class="role"\n    id="red"></div>',
+        opts: { 'attr-new-line': 1 },
+        output: 0
+    },
+    {
         desc: 'should match tags with attributes splitted on multiple lines if first row has attributes more then provided in rule',
         input: '<div class="role"\n    id="red"></div>',
         opts: { 'attr-new-line': 0 },
         output: 1
     },
     {
-        desc: 'should not match tags with attributes splitted on multiple lines if first row has attributes less or equal then provided in rule',
-        input: '<div\nclass="role"\nid="red">\n</div>',
-        opts: { 'attr-new-line': 0 },
+        desc: 'should not match tags with one attribute in the row for +0',
+        input: '<div class="role">\n</div>',
+        opts: { 'attr-new-line': "+0" },
         output: 0
     },
     {
-        desc: 'should not match tags with attributes splitted on multiple lines if first row has attributes less or equal then provided in rule',
-        input: '<div class="role"\n    id="red"></div>',
-        opts: { 'attr-new-line': 1 },
+        desc: 'should not match tags with one attribute in the row and wrapper for +0',
+        input: '<html>\n<body>\n    <div class="role"></div>\n</body>\n</html>',
+        opts: { 'attr-new-line': "+0" },
         output: 0
+    },
+    {
+        desc: 'should match tags with two attribute in the row and wrapper for +0',
+        input: '<html>\n<body>\n    <div class="role" id="me"></div>\n</body>\n</html>',
+        opts: { 'attr-new-line': "+0" },
+        output: 1
     }
 ];
