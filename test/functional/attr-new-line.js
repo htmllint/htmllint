cced89977155770caccc3ce2e3d6c8af26d63140
Oleksandr Knyga <oleksandrknyga@gmail.com>
2015-08-19 17:35:27 +0200

    fix travis linter issues

--- test/functional/attr-new-line.js
+++ test/functional/attr-new-line.js
@@ -32,27 +32,27 @@ module.exports = [
     }, {
         desc: 'should not match tags with one attribute in the row for +0',
         input: '<div class="role">\n</div>',
-        opts: { 'attr-new-line': "+0" },
+        opts: { 'attr-new-line': '+0' },
         output: 0
     }, {
         desc: 'should not match tags with one attribute in the row and wrapper for +0',
         input: '<html>\n<body>\n    <div class="role"></div>\n</body>\n</html>',
-        opts: { 'attr-new-line': "+0" },
+        opts: { 'attr-new-line': '+0' },
         output: 0
     }, {
         desc: 'should match tags with two attribute in the row and wrapper for +0',
         input: '<html>\n<body>\n    <div class="role" id="me"></div>\n</body>\n</html>',
-        opts: { 'attr-new-line': "+0" },
+        opts: { 'attr-new-line': '+0' },
         output: 1
     }, {
         desc: 'should not match tags with two attribute in the row and with no-value attr for +0',
         input: '<phone-preview-ios\n        id="dsa"\n        trasclude>\n</phone-preview-ios>',
-        opts: { 'attr-new-line': "+0" },
+        opts: { 'attr-new-line': '+0' },
         output: 0
     }, {
         desc: 'should not match tags with 3 boolean attributes for',
         input: '<picker-date\n            ng-model="campaignUiData.end_date"\n            name="endDate"\n            rome-options="endDateOption"\n            required\n            romemin\n            romemax>\n          </picker-date>',
-        opts: { 'attr-new-line': "+0" },
+        opts: { 'attr-new-line': '+0' },
         output: 0
     }
 ];
