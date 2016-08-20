16364c7f5105aa97422543e0e7e07175da29fa89
Joao Dias <mail@joaodias.eu>
2016-08-20 17:50:40 +0200

    feat(class-no-dup-split-separator): add a custom separator option for class-no-dup

--- test/functional/class-no-dup.js
+++ test/functional/class-no-dup.js
@@ -22,5 +22,19 @@ module.exports = [
         input: '<body><div><p class="hey hey hi ho ho">Text</p></div></body>',
         opts: { 'class-no-dup': true },
         output: 2
+    },
+
+    // class-no-dup-split-separator
+    {
+        desc: 'should catch duplicates with a custom separator',
+        input: '<body><div><p class="hey hey">Text</p></div></body>',
+        opts: { 'class-no-dup': true, 'class-no-dup-split-separator': /\s+(?![^{]*})/ },
+        output: 1
+    },
+    {
+        desc: 'should catch multiple duplicates with a custom separator',
+        input: '<body><div><p class="hey {foo bar} hey hi {foo bar}">Text</p></div></body>',
+        opts: { 'class-no-dup': true, 'class-no-dup-split-separator': /\s+(?![^{]*})/ },
+        output: 2
     }
 ];
