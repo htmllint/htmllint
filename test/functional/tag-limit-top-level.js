cac58d542bc3bd22020ff16098bb5ad9c645fb28
Marshall Lochbaum <mwlochbaum@gmail.com>
2017-01-14 14:29:45 -0500

    Rename html-content-model to tag-limit-top-level

--- test/functional/tag-limit-top-level.js
+++ test/functional/tag-limit-top-level.js
@@ -1,38 +1,38 @@
 module.exports = [
     {
-        desc: 'html-div should pass when html-content-model set to false',
+        desc: 'html-div should pass when tag-limit-top-level set to false',
         input: '<html><div></div></html>',
-        opts: { 'html-content-model': false },
+        opts: { 'tag-limit-top-level': false },
         output: 0
     }, {
-        desc: 'html-div should fail when html-content-model set to true',
+        desc: 'html-div should fail when tag-limit-top-level set to true',
         input: '<html><div></div></html>',
-        opts: { 'html-content-model': true },
+        opts: { 'tag-limit-top-level': true },
         output: 1
     }, {
         desc: 'head should pass when no html is present',
         input: '<head></head>',
-        opts: { 'html-content-model': true },
+        opts: { 'tag-limit-top-level': true },
         output: 0
     }, {
         desc: 'html-head-body should pass',
         input: '<html><head></head><body></body></html>',
-        opts: { 'html-content-model': true },
+        opts: { 'tag-limit-top-level': true },
         output: 0
     }, {
         desc: 'html-head-div-body should fail',
         input: '<html><head></head><div></div><body></body></html>',
-        opts: { 'html-content-model': true },
+        opts: { 'tag-limit-top-level': true },
         output: 1
     }, {
         desc: 'should fail for each illegal element',
         input: '<html><div></div><head></head><div></div><body></body></html>',
-        opts: { 'html-content-model': true },
+        opts: { 'tag-limit-top-level': true },
         output: 2
    }, {
         desc: 'html-head-body should pass',
         input: '<html><head></head><body><div></div></body></html>',
-        opts: { 'html-content-model': true },
+        opts: { 'tag-limit-top-level': true },
         output: 0
     }
 ];
