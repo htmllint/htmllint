24b202851664cacfd8daf99250e3201510d5c187
Marshall Lochbaum <mwlochbaum@gmail.com>
2017-01-20 23:23:15 -0500

    Rewrite spec-char-escape

--- test/functional/spec-char-escape.js
+++ test/functional/spec-char-escape.js
@@ -4,17 +4,20 @@ module.exports = [
         input: '<div><p>Hello & hello</p></div>',
         opts: { 'spec-char-escape': false },
         output: 0
-    },
-    {
+    }, {
         desc: 'should recognize special characters inside of text elements',
         input: '<div><p class="className" id="IdName" label="label" >Hello & hello</p></div>',
         opts: { 'spec-char-escape': true },
         output: 1
-    },
-    {
+    }, {
         desc: 'should recognize special characters in attribute values',
         input: '<div><p id="mine&ours" class="yours>mine"></p></div>',
         opts: { 'spec-char-escape': true },
         output: 1
+    }, {
+        desc: 'should return an error for each invalid escape',
+        input: '<div><p id="john&*;paul&;ringo&george^"></p></div>',
+        opts: { 'spec-char-escape': true },
+        output: 3
     }
 ];
