d7f1fc409a6a0e7b5e1eae4455bf14d3972d3231
Marshall Lochbaum <mwlochbaum@gmail.com>
2017-08-26 18:05:15 +0100

    Suppress attributes matching multiple entries in attr-order

--- test/functional/attr-order.js
+++ test/functional/attr-order.js
@@ -34,5 +34,47 @@ module.exports = [
         input: '<img src="test.gif" class="test"></img>',
         opts: { 'attr-order': ['class','src','height','width'] },
         output: 1
+    },
+    {
+        desc: 'should pass when attribute order is consistent with regex (class then everything else)',
+        input: '<img class="test" src="test.gif" height="200" width="300"></img>',
+        opts: { 'attr-order': ['class','/^.*$/'] },
+        output: 0
+    },
+    {
+        desc: 'should fail when attribute order is not consistent with regex (class then everything else)',
+        input: '<img src="test.gif" class="test" height="200" width="300"></img>',
+        opts: { 'attr-order': ['class','/^.*$/'] },
+        output: 1
+    },
+    {
+        desc: 'should give one error per misplaced attribute with regex (class then everything else)',
+        input: '<img src="test.gif" height="200" class="test" width="300"></img>',
+        opts: { 'attr-order': ['class','/^.*$/'] },
+        output: 2
+    },
+    {
+        desc: 'should pass when attribute order is consistent with regex (class then data tags)',
+        input: '<img class="test" src="test.gif" data-x="123" data-y="456"></img>',
+        opts: { 'attr-order': ['class','/^data-.*$/'] },
+        output: 0
+    },
+    {
+        desc: 'should fail when attribute order is not consistent with regex (class then data tags)',
+        input: '<img src="test.gif" data-x="123" class="test" data-y="456"></img>',
+        opts: { 'attr-order': ['class','/^data-.*$/'] },
+        output: 1
+    },
+    {
+        desc: 'should pass when attribute order is consistent with regex (class then data tags then others)',
+        input: '<img class="test" data-x="123" data-y="456" src="test.gif"></img>',
+        opts: { 'attr-order': ['class','/^data-.*$/','/^.*$/'] },
+        output: 0
+    },
+    {
+        desc: 'should fail when attribute order is not consistent with regex (class then data tags then others)',
+        input: '<img class="test" src="test.gif" data-x="123" data-y="456"></img>',
+        opts: { 'attr-order': ['class','/^data-.*$/','/^.*$/'] },
+        output: 1
     }
 ];
