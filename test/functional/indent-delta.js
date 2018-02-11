ea71ab48ea0cbd5b5d2cf86b26115a858e027af4
Marshall Lochbaum <mwlochbaum@gmail.com>
2018-02-11 14:22:07 +0000

    Fix some potential issues with strange whitespace in indent-delta

--- test/functional/indent-delta.js
+++ test/functional/indent-delta.js
@@ -29,6 +29,29 @@ module.exports = [
         opts: { 'indent-delta':true, 'indent-width':2 },
         output: 0
     }, {
+        desc: 'should tab to next multiple of indent-width',
+        input: [
+            '<body>',
+            '\t<p>hello</p>',         // 1 tab
+            '   \t   \t<p>hello</p>', // 2 tabs
+            '    <p>hello</p>',       // 1 tab
+            '</body>'
+        ].join('\n'),
+        opts: { 'indent-delta':true, 'indent-width':4 },
+        output: 1 // From indent-width; none from indent-delta
+    }, {
+        desc: 'should ignore empty lines',
+        input: [
+            '<body>',
+            '\t<p>break with</p>',
+            '\t\t<p>empty line</p>',
+            '',
+            '\t<p>is fine</p>',
+            '</body>'
+        ].join('\r\n'),
+        opts: { 'indent-delta':true, 'indent-width':6 },
+        output: 0
+    }, {
         desc: 'should detect bad indent with tabs and spaces',
         input: [
             '<body>',
