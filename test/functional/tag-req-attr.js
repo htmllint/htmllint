e2597b724a2350496460255b7bfebe17fd33c181
Marshall Lochbaum <mwlochbaum@gmail.com>
2018-02-11 18:04:34 +0000

    Change tabs to spaces

--- test/functional/tag-req-attr.js
+++ test/functional/tag-req-attr.js
@@ -3,102 +3,102 @@ module.exports = [
         desc: 'should pass when set to false',
         input: '<img />',
         opts: {
-			'tag-req-attr': false
-		},
+            'tag-req-attr': false
+        },
         output: 0
     },
     {
         desc: 'should pass when set to empty object',
         input: '<img />',
         opts: {
-			'tag-req-attr': {}
-		},
+            'tag-req-attr': {}
+        },
         output: 0
     }, {
         desc: 'should pass when tag has correct attributes set',
         input: '<img src="nyan.mrw" alt="nyan" />',
         opts: {
-			'tag-req-attr': {
-				'img': [
-					{
-						'name': 'src'
-					},
-					{
-						'name': 'alt'
-					}
-				]
-			}
-		},
+            'tag-req-attr': {
+                'img': [
+                    {
+                        'name': 'src'
+                    },
+                    {
+                        'name': 'alt'
+                    }
+                ]
+            }
+        },
         output: 0
     }, {
         desc: 'should fail when tag has no correct attributes set',
         input: '<img id="notasource" />',
-		opts: {
-			'tag-req-attr': {
-				'img': [
-					{
-						'name': 'src'
-					}
-				]
-			}
-		},
+        opts: {
+            'tag-req-attr': {
+                'img': [
+                    {
+                        'name': 'src'
+                    }
+                ]
+            }
+        },
         output: 1
     }, {
         desc: 'should fail when tag has no correct attributes set',
         input: '<img id="notasource" />',
-		opts: {
-			'tag-req-attr': {
-				'img': [
-					{
-						'name': 'src',
-						'allowEmpty': false
-					}
-				]
-			}
-		},
+        opts: {
+            'tag-req-attr': {
+                'img': [
+                    {
+                        'name': 'src',
+                        'allowEmpty': false
+                    }
+                ]
+            }
+        },
         output: 1
     }, {
         desc: 'should fail when tag attribute is empty',
         input: '<img src />',
-		opts: {
-			'tag-req-attr': {
-				'img': [
-					{
-						'name': 'src'
-					}
-				]
-			}
-		},
+        opts: {
+            'tag-req-attr': {
+                'img': [
+                    {
+                        'name': 'src'
+                    }
+                ]
+            }
+        },
         output: 1
     }, {
         desc: 'should pass when tag has correct attributes set',
         input: '<img src="nyan.mrw" alt="" />',
         opts: {
-			'tag-req-attr': {
-				'img': [
-					{
-						'name': 'src'
-					},
-					{
-						'name': 'alt',
-						'allowEmpty': true
-					}
-				]
-			}
-		},
+            'tag-req-attr': {
+                'img': [
+                    {
+                        'name': 'src'
+                    },
+                    {
+                        'name': 'alt',
+                        'allowEmpty': true
+                    }
+                ]
+            }
+        },
         output: 0
     }, {
         desc: 'should pass when there is no configuration for the tag',
         input: '<img src="nyan.mrw" alt="" />',
         opts: {
-			'tag-req-attr': {
-				'input': [
-					{
-						'name': 'type'
-					}
-				]
-			}
-		},
+            'tag-req-attr': {
+                'input': [
+                    {
+                        'name': 'type'
+                    }
+                ]
+            }
+        },
         output: 0
     }
 ];
